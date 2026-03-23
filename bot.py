860b5ba20f57ba25da61e9a38e15bcfef57c9469
abolfazltaqizadeh12345-lang <abolfazltaqizadeh12345@gmail.com>
2026-03-24 00:49:11 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
 import random
-import os
 
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, MessageHandler, CommandHandler, CallbackQueryHandler, filters, ContextTypes
 
-TOKEN = os.environ["TOKEN"]
+TOKEN = "8650092381:AAHkP3bcqs1sNby-XPzjn-njLnnGgH_zyCM"
 
 # هندلر /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
